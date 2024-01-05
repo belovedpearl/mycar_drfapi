7735172f682a7100a743c87bc5dc48c8bb9aea1f
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-05 16:05:25 +0000

    Add ProfileDetail view and update the path

--- profiles/views.py
+++ profiles/views.py
@@ -11,3 +11,12 @@ class ProfileList(generics.ListAPIView):
     """
     queryset = Profile.objects.all().order_by('-created_at')
     serializer_class = ProfileSerializer
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile
+    Allows update of the profile
+    """
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()

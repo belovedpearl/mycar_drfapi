8c92769eaacbf5a415b40f1096ebcba7cb3c65bc
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-06 17:42:25 +0000

    Add condition to create upvote to view

--- upvotes/views.py
+++ upvotes/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics, permissions
 from upvotes.models import Upvote
 from upvotes.serializers import UpvoteSerializer
 from mycar_drfapi.permissions import IsOwnerOrReadOnly
+from downvotes.models import Downvote
+from rest_framework import serializers
 
 
 class UpvoteListView(generics.ListCreateAPIView):
@@ -13,6 +15,13 @@ class UpvoteListView(generics.ListCreateAPIView):
     queryset = Upvote.objects.all()
 
     def perform_create(self, serializer):
+        user = self.request.user
+        post = serializer.validated_data['post']
+        # Check if the user has already downvoted
+        if Downvote.objects.filter(owner=user, post=post).exists():
+            raise serializers.ValidationError(
+                {'details': 'Cannot upvote because you already downvoted this post'}
+            )
         serializer.save(owner=self.request.user)
 
 

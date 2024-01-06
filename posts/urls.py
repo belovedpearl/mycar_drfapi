af1093a303275c99da8df11cdb5fb7ff9c6e97f7
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-06 03:55:03 +0000

    Add PostDetail view and update the path

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,7 @@ from posts import views
 
 urlpatterns = [
    path('posts/', views.PostsList.as_view()),
+   path("posts/<int:pk>/", views.PostDetail.as_view()),
    
     
 ]
--- posts/views.py
+++ posts/views.py
@@ -17,3 +17,12 @@ class PostsList(generics.ListCreateAPIView):
     
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it if you own it.
+    """
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Post.objects.all().order_by('-created_at')

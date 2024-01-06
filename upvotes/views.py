0fccfd30f743bb5bb844b4938782077f9079366b
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-06 16:53:51 +0000

    Add view to access the upvote details and user restriction

--- upvotes/views.py
+++ upvotes/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from upvotes.models import Upvote
 from upvotes.serializers import UpvoteSerializer
+from mycar_drfapi.permissions import IsOwnerOrReadOnly
 
 
 class UpvoteListView(generics.ListCreateAPIView):
@@ -13,3 +14,12 @@ class UpvoteListView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class UpvoteDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve an upvote or delete if user is the owner
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = UpvoteSerializer
+    queryset = Upvote.objects.all()

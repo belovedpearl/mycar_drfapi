21ab1c9b80cb610074de60fff1b511971d1708a1
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-06 16:37:29 +0000

    Add serializers and view to list upvotes

---
+++ upvotes/serializers.py
@@ -0,0 +1,26 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from upvotes.model import Upvote
+
+
+class UpvoteSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Upvote model
+    Owner field is ReadOnly to prevent change
+    Allows one vote on a post via the integrity constraint
+    """
+    
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Upvote
+        fields = ['id', 'created_at', 'owner', 'car']
+
+    def create(self, validated_data):
+       
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError(
+                {'details': 'Your vote is already registered'}
+            )
--- upvotes/views.py
+++ upvotes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from upvotes.model import Upvote
+from upvote.serializers import UpvoteSerializer
 
-# Create your views here.
+
+class UpvoteListView(generics.ListCreateAPIView):
+    """
+    Lists or create Upvotes
+    """
+    serializer_class = UpvoteSerializer  
+    queryset = Upvote.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)

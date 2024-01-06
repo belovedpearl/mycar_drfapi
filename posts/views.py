e78db6b04fb1de6f48c80054e85f952104184837
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-06 03:29:33 +0000

    Add PostList view in the post app

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from mycar_drfapi.permissions import IsOwnerOrReadOnly
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostsList(generics.ListCreateAPIView):
+    """
+    Checks for permission allowed on user
+    Gets all Post objects in a list
+    Associates owner with the post
+    """
+   
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.all().order_by('-created_at')
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)

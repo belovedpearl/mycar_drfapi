7a29169fef887490b60129708e21005444ddbf25
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-06 05:56:01 +0000

    Add review serializer, view and path

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from reviews.models import Review
+from .serializers import ReviewSerializer
 
-# Create your views here.
+
+
+class ReviewList(generics.ListCreateAPIView):
+    """
+    List reviews or create a review if logged in.
+    """
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Review.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)

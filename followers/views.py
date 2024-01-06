4e72899617978e327b6f30d42788db6378cd76ef
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-06 18:30:55 +0000

    Add follow serializers and views

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from mycar_drfapi.permissions import IsOwnerOrReadOnly
+from followers.models import Follower
+from followers.serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowersListView(generics.ListCreateAPIView):
+    """
+    List follower or create a follower if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowerSerializer  
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a follower or delete it if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()

351f27ba075124c36091714cbf1a4d378337d3e7
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-08 04:21:26 +0000

    Add count fields to profile

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,17 +1,34 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     """
     Serializes Profile instances for right API responses
+    Get the owner from the request context
+    Get following status and id from request context
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
+    upvotes_count = serializers.ReadOnlyField()
+    downvotes_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_following_id(self, obj):
+        user = self.context["request"].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(owner=user, followed=obj.owner).first()
+            # print(following)
+            return following.id if following else None
+        return None
 
 
     class Meta:
@@ -19,5 +36,7 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
             'about', 'image', 'is_owner', 'current_employer',
-             'job_title', 'location'
+             'job_title', 'location', 'following_id', 'posts_count',
+              'followers_count', 'following_count', 'upvotes_count',
+               'downvotes_count',
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from profiles.models import Profile
 from profiles.serializers import ProfileSerializer
 from mycar_drfapi.permissions import IsOwnerOrReadOnly
+from django.db.models import Count
 
 
 class ProfileList(generics.ListAPIView):
@@ -11,7 +12,14 @@ class ProfileList(generics.ListAPIView):
     No create view as profile creation is handled by signals
     Orders profile list by creation in descending order
     """
-    queryset = Profile.objects.all().order_by('-created_at')
+    # queryset = Profile.objects.all().order_by('-created_at')
+    queryset = Profile.objects.annotate(
+        posts_count = Count('owner__post', distinct=True),
+        followers_count = Count('owner__followed', distinct=True),
+        following_count = Count('owner__following', distinct=True),
+        upvotes_count = Count('owner__upvote', distinct=True),
+        downvotes_count = Count('owner__downvote', distinct=True),
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
 
 

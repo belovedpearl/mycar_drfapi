3f05f92fc5c23372cbe03ca99b6386b2f294598f
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-08 05:47:15 +0000

    Add count field for votes and reviews

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from posts.models import Post
+from upvotes.models import Upvote
+from downvotes.models import Downvote
+from reviews.models import Review
 
 class PostSerializer(serializers.ModelSerializer):
     """
@@ -13,6 +16,11 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source = 'owner.profile.id')
     location = serializers.ReadOnlyField(source = 'owner.profile.location')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    upvote_id = serializers.SerializerMethodField()
+    downvote_id = serializers.SerializerMethodField()
+    upvotes_count = serializers.ReadOnlyField()
+    downvotes_count = serializers.ReadOnlyField()
+    reviews_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -33,12 +41,30 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_upvote_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            upvote = Upvote.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return upvote.id if upvote else None
+        return None
+    
+    def get_downvote_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            downvote = Downvote.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return downvote.id if downvote else None
+        return None
+
     class Meta:
         model = Post
-        fields = '__all__'
-   
-        # fields = [
-        # 'id', 'owner', 'is_owner', 'profile_id',
-        #  'profile_image', 'make', 'model',
-        #   'year', 'description', 'image',
-        # ]
+        fields = [
+        'id', 'owner', 'is_owner', 'profile_id', 'location',
+         'profile_image', 'make', 'model','year', 'description',
+          'image', 'body_types', 'created_at', 'updated_at',
+           'upvote_id', 'downvote_id', 'upvotes_count',
+            'downvotes_count', 'reviews_count'
+        ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from mycar_drfapi.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
+from django.db.models import Count
 
 
 class PostsList(generics.ListCreateAPIView):
@@ -13,7 +14,11 @@ class PostsList(generics.ListCreateAPIView):
    
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all().order_by('-created_at')
+    queryset = Post.objects.annotate(
+        upvotes_count= Count('upvotes', distinct=True),
+        downvotes_count= Count('downvotes', distinct=True),
+        reviews_count= Count('review', distinct=True)
+    ).order_by('-created_at')
     
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)

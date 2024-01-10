7acb7f093d31a630f33e3fbd3dd28556572ac2e8
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-10 10:54:05 +0000

    Add cookies configurations setting

--- mycar_drfapi/settings.py
+++ mycar_drfapi/settings.py
@@ -67,6 +67,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-belovedpear-mycardrfapi-pzdbdqxv52n.ws-eu107.gitpod.io',
+    'localhost',
+    'mycardrfapi.herokuapp.com'
 ]
 
 
@@ -90,6 +92,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    'corsheaders',
 
 
     'profiles',
@@ -102,6 +105,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -110,6 +114,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+if 'CLIENT_ORIGIN' in os.environ:
+     CORS_ALLOWED_ORIGINS = [
+         os.environ.get('CLIENT_ORIGIN')
+     ]
+else:
+     CORS_ALLOWED_ORIGIN_REGEXES = [
+         r"^https://.*\.gitpod\.io$",
+     ]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'mycar_drfapi.urls'
 
